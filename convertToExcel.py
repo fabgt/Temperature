098f9f0586d55a05126976c1895abfd72e0df95e
Fabgt <fabienclement.desvignes@gmail.com>
2019-04-01 17:20:22 +0000

    clean convertToExcel file

--- convertToExcel.py
+++ convertToExcel.py
@@ -5,12 +5,11 @@ import xlsxwriter
 
 def ExtractDateAndTempPosition(stringFromFile):
  "Extract temperature and date positions in string from file"
- #dateStart=0
- #dateEnd=0
 
  global indexDateMax
  indexDateMax = len(stringFromFile)
  print("indexDateMax: ",indexDateMax)
+ #Find index start Date
  for indexDate in range(0, (indexDateMax-1)):
   print(indexDate)
   if stringFromFile[indexDate]=='[':
@@ -18,18 +17,14 @@ def ExtractDateAndTempPosition(stringFromFile):
    dateStart=indexDate+1
    print("dateStart index is equal to: ", dateStart)
    break
-  #else:
-   #print("Error in extract dateStart position")
 
+ #Find index end Date
  for indexDateBis in range(0, (indexDateMax-1)):
   if stringFromFile[indexDateBis]==',':
    global dateEnd
    dateEnd=indexDateBis
    print("dateEnd index is equal to: ", dateEnd)
    break
-  #else:
-   #print("Error in extract dateEnd position")
-   #Error
 
  #Determine temperature first and last character position
  global tempStart
@@ -38,13 +33,9 @@ def ExtractDateAndTempPosition(stringFromFile):
  for indexTemp in range(0, (indexDateMax-1)):
   if stringFromFile[indexTemp]==']':
    global tempEnd
-   tempEnd=indexTemp-1
+   tempEnd=indexTemp
    print("tempEnd index is equal to: ",tempEnd)
    break
-  #else:
-   #Error
-   #print("Error in extract temp position")
- #print(dateStart, dateEnd, tempStart, tempEnd)
 
 def ExtractDateAndTemp(stringFromFile,dateStart,dateEnd,tempStart,tempEnd):
  "Determine value of date and temperature"
@@ -52,20 +43,19 @@ def ExtractDateAndTemp(stringFromFile,dateStart,dateEnd,tempStart,tempEnd):
  dateRawValue=''
  global tempValue
  tempValue=''
+ #Build dateRawValue
  for i in range (dateStart, dateEnd):
   dateRawValue=dateRawValue+stringFromFile[i]
  print("dateRawValue is equal to :",dateRawValue)
+ #Build tempValue
  for j in range (tempStart, tempEnd):
   tempValue=tempValue+stringFromFile[j]
  print("tempValue is equal to :",tempValue)
- #return dateRawValue,tempValue
 
 def ConvertRawDateToDate(dateRawValue):
  "Convert Raw Date to readable excel format date"
  global dateValue
  dateValue=dateRawValue
-
- #return dateValue
 
 def CopyDateAndTempToExcel(columnDate,columnTemp,row,dateValue,tempValue):
  "Copy each date and temperature logged in text file to excel file"
@@ -74,13 +64,7 @@ def CopyDateAndTempToExcel(columnDate,columnTemp,row,dateValue,tempValue):
  worksheet.write(cellDate,dateValue)
  worksheet.write(cellTemp,tempValue)
 
-
 #MAIN#
-#Read lines of text file
-#fp=open("textfile.txt",'r')
-#data = fp.read()
-#fp.close()
-
 columnDate='A'
 columnTemp='B'
 row=1
