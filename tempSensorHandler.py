fa76a017212ec0c200b19d789b7ba7b362e114e3
Fabgt <fabienclement.desvignes@gmail.com>
2020-01-11 12:43:44 +0000

    Added second sensor and working

--- tempSensorHandler.py
+++ tempSensorHandler.py
@@ -2,35 +2,42 @@
 #Import Libraries
 import os
 import glob
-import time
+import datetime
 import numpy as np
-from datetime import datetime
+import httplib
+import urllib
+import time
 
 #Initialize GPIO Pins
 os.system('modprobe w1-gpio') #turns on the gpio module
 os.system('modprobe w1-therm') #turns on the temperature module
 
+#Define thingspeak API key
+key = "E2TRNT32ZP9DQEG1"
+
 #Find the correct device file that holds the temperature data
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+#base_dir = '/sys/bus/w1/devices/'
+#device_folder = glob.glob(base_dir + '28*')[0]
+#device_file = device_folder + '/w1_slave'
+device1_file = '/sys/bus/w1/devices/28-0316471d4dff/w1_slave'
+device2_file = '/sys/bus/w1/devices/28-03164764a3ff/w1_slave'
 
 #A function that reads the sensors data
-def read_temp_raw():
+def read_temp_raw(device_file):
  f=open(device_file, 'r') #Opens the temperature device file
  lines=f.readlines() #Returns the text
  f.close()
  return lines
 
 #Convert the value of the sensor into a temperature
-def read_temp():
- lines=read_temp_raw() #Read the temperature 'device file'
+def read_temp(device_file):
+ lines=read_temp_raw(device_file) #Read the temperature 'device file'
 
  #While the first line does not contain 'YES', wait for 0.2s
  #and then read the device file again.
  while lines[0].strip()[-3:]!='YES':
   time.sleep(0.2)
-  lines = read_temp_raw()
+  lines = read_temp_raw(device_file)
 
  #Look for the position of the '=' in the second line of the
  #device file.
@@ -46,23 +53,29 @@ def read_temp():
 
 #Print out the temperature until the program is stopped.
 with open('textfile.txt','w') as destination:
+ print('Temperature being logged.')
  while True:
-  time_stamp=time.time()
-  data=[time_stamp,read_temp()]
-  print(data)
+  time_stamp=str(datetime.datetime.now())
+  data1=[time_stamp,read_temp(device1_file)]
+  #print(data)
+  destination.write('%s\n' % data1)
+  temp1 = read_temp(device1_file)
+  temp2 = read_temp(device2_file)
 
- #with open('textfile.txt','w') as destination:
+  #Send data to thingspeak
+  params = urllib.urlencode({'field1':temp1,'field2':temp2,'key':key})
+  headers = {"Content-typZZe":"application/x-www-form-urlencoded","Accept":"text/plain"}
+  conn = httplib.HTTPConnection("api.thingspeak.com:80")
+  try:
+    conn.request("POST","/update",params,headers)
+    response = conn.getresponse()
+    dataResponse = response.read()
+    #print temp1,temp2
+    #print response.status #,reponse.reason
+    conn.close()
+  except:
+    print "Thingspeak connection failed"
 
-#for line in destination:
- 	#	print(line)	#do nothing
-  destination.write('%s\n' % data)
-
- # try:
-	#header="Temperature\n"
-	#header+="This is a second line"
-	#np.savetxt('textfile.txt',data,fmt='%.3e',newline='\n', header=header)
- #except KeyboardInterrupt:
- #	print('You cancelled the operation')
-
- time.sleep(1)
+  #Wait 5 min before sending again
+  time.sleep(30)
 
